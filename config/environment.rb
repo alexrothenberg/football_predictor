66cb730034a1f92109f9b5a236883286266cdb07
Matthew Deiters <bangkok@bangkok.local>
2010-04-29 07:17:28 -0700

    fixing authlogic for heroku

--- config/environment.rb
+++ config/environment.rb
@@ -1,10 +1,10 @@
-require 'authlogic'
-
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
+require 'authlogic'
+
 
 Rails::Initializer.run do |config|
   # Settings in config/environments/* take precedence over those specified here.
