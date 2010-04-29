f6b95efcdf1987d93bb95a4efc8391c49e1f9361
Matthew Deiters <bangkok@bangkok.local>
2010-04-29 07:14:33 -0700

    fixing authlogic for heroku

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,4 @@
-# Be sure to restart your server when you modify this file
+require 'authlogic'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.5' unless defined? RAILS_GEM_VERSION
